87eedd9da8ee6e0f2ac91bf5b75fb7e4529f8d7b
Yury <edem_garden@mail.ru>
2017-01-31 14:33:42 -0500

    Draw grid is moved to outside of loop

--- ttt.c
+++ ttt.c
@@ -10,11 +10,20 @@ void DrawGrid (SDL_Renderer* r, int x, int y, int size);
 
 int main (void)
 {
+    SDL_Window *win;
+    SDL_Renderer *rend;
+    
     SDL_Init (SDL_INIT_VIDEO);
-    SDL_Window *win = SDL_CreateWindow("Tic-tac-toe", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, RES_X, RES_Y, 0);
-    SDL_Renderer *rend = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
+    win = SDL_CreateWindow("Tic-tac-toe", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, RES_X, RES_Y, 0);
+    rend = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
     
-    
+    SDL_RenderClear(rend);
+    SDL_RenderPresent(rend);
+        
+    // Draw grid 
+    DrawGrid (rend, (RES_X - 3 * GRIDSIZE) / 2, (RES_Y - 3 * GRIDSIZE) / 2, GRIDSIZE);
+    SDL_RenderPresent(rend);
+        
     while(1)
     {
         SDL_Event e;
@@ -25,14 +34,6 @@ int main (void)
                 break;
         }
         
-        SDL_RenderClear(rend);
-        SDL_RenderPresent(rend);
-        
-        // Draw grid 
-        DrawGrid (rend, (RES_X - 3 * GRIDSIZE) / 2, (RES_Y - 3 * GRIDSIZE) / 2, GRIDSIZE);
-
-        SDL_RenderPresent(rend);
-        SDL_Delay(1000 / 60);
     }
     
     
@@ -42,10 +43,13 @@ int main (void)
     
 }
 
+// Function to draw the game grid
+// Args: render, coorinates of left up corner, size of cell side
 
 void DrawGrid (SDL_Renderer* r, int x, int y, int size)
 {
-    for (int i = 0; i < 4 * size; i += size)
+    //for (int i = 0; i < 4 * size; i += size) // with border
+    for (int i = size; i < 3 * size; i += size) // without border
     {
         SDL_RenderDrawLine(r, x, y+i, x+3*size, y+i);
         SDL_RenderDrawLine(r, x+i, y, x+i, y+3*size);           
