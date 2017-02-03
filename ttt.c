61e6a64fc301a6decdd9eedd90d6e57bf98085fc
Yury <123@gmail.com>
2017-02-03 07:10:25 -0500

    Add comp move in test mode

--- ttt.c
+++ ttt.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include <SDL2/SDL.h>
 #include <math.h>
 
@@ -63,12 +65,15 @@ int main (void)
     
     DrawGrid (rend, mygrid);
     SDL_RenderPresent(rend);
+    int player_move = 1;
+    
 //===================================================
 //================ Main loop  =======================
 //===================================================
     while(1)
     {
-       // Checking win
+
+    // ---------- Checking win --------------
         if (CheckWin (mygrid, 1)) {
             printf("Cross win\n");
             break;
@@ -77,81 +82,102 @@ int main (void)
             printf("Zero win\n");
             break;
         }
-        
-        
+
+    // ----------- Check quit ----------------        
+
         if (SDL_PollEvent(&e))
         {
             if (e.type == SDL_QUIT)
                 break;
         }
         
-        //Catch the mouse
-        if (e.type == SDL_MOUSEBUTTONDOWN)
+    // ----------- Computer move --------------
+   
+        if (!player_move)
         {
-            int mx, my;
-            SDL_GetMouseState(&mx, &my);
-           
-        // Check if mouse coordinates in any cell 
-                       
-             for (int i = 0; i < 3; i++)
-             {
-                 for (int j = 0; j < 3; j++)
-                 {    
-                     if 
-                     (
-                         (mx > mygrid.cellcenter[i][j].x - mygrid.cellsize/2 && my > mygrid.cellcenter[i][j].y- mygrid.cellsize/2) &&
-                         (mx < mygrid.cellcenter[i][j].x + mygrid.cellsize/2 && my < mygrid.cellcenter[i][j].y + mygrid.cellsize/2)
-                     )
-                     // then set sign
-                     {
-                         if (e.button.button == SDL_BUTTON_LEFT)
-                             mygrid.cellsign[i][j] = 1;
-                         else
-                             mygrid.cellsign[i][j] = 2;
-                     }  
-              
-                        
-                          
-                 }
-              }
-
+            int n, i, j;
+        //Random for testing...
+            do {
+                srand(time(NULL));
+                n = rand()%9;
+                j = n % 3;
+                i = n / 3;
+                
+            } while (mygrid.cellsign[i][j] != 0);
+            
+            mygrid.cellsign [i][j] = 2;
+            
             SDL_RenderClear(rend);
             DrawGrid(rend, mygrid);
             SDL_RenderPresent(rend);
             
+            player_move = 1;
+                    
+        }
+        
+    // ------------ Player move ---------------
+        else 
+        {
+            SDL_WaitEvent(&e);
+
+            if (e.type == SDL_MOUSEBUTTONDOWN)
+            {
+                int mx, my;
+                SDL_GetMouseState(&mx, &my);
+           
+            // Check if mouse coordinates in any cell 
+                       
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {    
+                        if 
+                         (
+                             (mx > mygrid.cellcenter[i][j].x - mygrid.cellsize/2 && my > mygrid.cellcenter[i][j].y - mygrid.cellsize/2) &&
+                             (mx < mygrid.cellcenter[i][j].x + mygrid.cellsize/2 && my < mygrid.cellcenter[i][j].y + mygrid.cellsize/2)
+                         )
+                         // then set sign
+                         {
+                             if (e.button.button == SDL_BUTTON_LEFT)
+                                mygrid.cellsign[i][j] = 1;
+                            //else
+                          //    mygrid.cellsign[i][j] = 2;
+                         
+                        // Reload grid
+                            SDL_RenderClear(rend);
+                            DrawGrid(rend, mygrid);
+                            SDL_RenderPresent(rend);
+                        // Give move to computer
+                            player_move = 0; 
+
+                        }
+                     }                            
+                 }
+              }
          }
         
     }
-    
-    
+        
     SDL_DestroyRenderer(rend);
     SDL_DestroyWindow(win);
     SDL_Quit();
     
 }
 
-/* Function to draw the game grid
-Args: render, coorinates of left up corner, size of cell side
-void DrawGrid (SDL_Renderer* r, int x, int y, int size)
-{
-    //for (int i = 0; i < 4 * size; i += size) // with border
-    for (int i = size; i < 3 * size; i += size) // without border
-    {
-        SDL_RenderDrawLine(r, x, y+i, x+3*size, y+i);
-        SDL_RenderDrawLine(r, x+i, y, x+i, y+3*size);           
-    }
-}*/
-
+/****************************************
+*************** FUNCTIONS ***************
+****************************************/
 
 // Function to draw the game grid V.2
 // Args: render, grid
 void DrawGrid (SDL_Renderer* r, struct Grid g)
 {
-    int x = g.center.x - g.cellsize * 1.5;
-    int y = g.center.y - g.cellsize * 1.5;
+    int x = g.center.x - g.cellsize * 1.5; // Get up left corner
+    int y = g.center.y - g.cellsize * 1.5; //
     int step = g.cellsize;
    
-    for (int i = 0; i < 4 * step; i += step) // with border
+    // Draw grid...
+    for (int i = 0; i < 4 * step; i += step)      // with border
     //for (int i = step; i < 3 * step; i += step) // without border
     {
         SDL_RenderDrawLine(r, x, y+i, x+3*step, y+i);
@@ -168,7 +194,6 @@ void DrawGrid (SDL_Renderer* r, struct Grid g)
                 DrawCross(r, g.cellcenter[i][j].x, g.cellcenter[i][j].y, g.cellsize / 3);
             else if (g.cellsign[i][j] == 2)
                 DrawCircle(r, g.cellcenter[i][j].x, g.cellcenter[i][j].y, g.cellsize / 3);
-           
            
             SDL_RenderDrawPoint(r, g.cellcenter[i][j].x, g.cellcenter[i][j].y);
         }
@@ -198,7 +223,7 @@ void DrawCross (SDL_Renderer* r, int cx, int cy, int size)
 }
 
 
-// Function checks win 1
+// Function checks win
 int CheckWin(struct Grid g, int winner)
 {
     int n = 0;
@@ -208,12 +233,10 @@ int CheckWin(struct Grid g, int winner)
     {
         for (int j = 0; j < 3; j++)
         {
-            if (g.cellsign[i][j] == winner)
-                n++;
+            if (g.cellsign[i][j] == winner)  n++;
         }
         
-        if (n == 3)
-            return 1;
+        if (n == 3) return 1;
         else n = 0;
     }
     
@@ -222,33 +245,26 @@ int CheckWin(struct Grid g, int winner)
     {
         for (int j = 0; j < 3; j++)
         {
-            if (g.cellsign[j][i] == winner)
-                n++;
+            if (g.cellsign[j][i] == winner) n++;
         }
 
-        if (n == 3)
-            return 1;
+        if (n == 3) return 1;
         else n = 0;
     }
     
     //Check 1st diagonal
     
     if (g.cellsign[0][0] == winner) n++;
-        
     if (g.cellsign[1][1] == winner) n++;
-        
     if (g.cellsign[2][2] == winner) n++;
     
-    if (n == 3)
-        return 1;
+    if (n == 3) return 1;
     else n = 0;
     
     //Check 2st diagonal
     
     if (g.cellsign[0][2] == winner) n++;
-        
     if (g.cellsign[1][1] == winner) n++;
-        
     if (g.cellsign[2][0] == winner) n++;
     
     if (n == 3)
